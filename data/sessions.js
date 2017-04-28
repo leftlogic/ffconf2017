a19a5d5eb7190eca51e50a676075516c9f3475ad
Remy Sharp <remy@remysharp.com>
2017-04-28 07:52:17 +0100

    feat: initial working spec design
    Note: content is not real

---
+++ data/sessions.js
@@ -0,0 +1,7 @@
+module.exports = [
+{
+  title: 'Next level CSS',
+  speaker: 'Rachel Andrew',
+  description: 'While we were all looking the other way browsers have been implementing CSS features that do things you might think you need JavaScript or at least a pre-processor to achieve. We’ll take a tour through what is landing in the CSS specifications and your browsers right now and in the very near future.'
+}
+]
