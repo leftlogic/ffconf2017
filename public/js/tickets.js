1e4c058ca04ecb3015d2c2f5e8d557ec08af40ab
Remy Sharp <remy@remysharp.com>
2017-06-23 16:58:39 +0100

    fix: fukititlldo

--- public/js/tickets.js
+++ public/js/tickets.js
@@ -1,12 +1,110 @@
-/* global document, window */
+
+/**
+
+Look, if you're here you're taking a look at what's going on,
+and that's cool, but seriously: this code is a dodgy hot mess.
+
+I've got another (private) repo going on where I'm trying to
+get this logic right, and I've done a fucktonne of reading
+around how the ZX Spectrum works internally. I've learnt about
+T-states, and how it runs at 3.5Mhz, and why the pilot signal
+exists and how it works, sync bits etc. Even that a zero binary
+bit is 855 clock cycles (T-state) and that a one is 1710.
+
+I digress, you're here to see some code. Well, below you will
+indeed find code, but you may lose your way as you continue
+to scroll.
+
+I've got an epic blog post that I've been working on that
+explains this all, and explains what I learnt. In fact, I'm
+even giving it as a talk later this year at Paris Web and
+Reasons.
+
+Anyway…I've rambled a bit in the hope that I can distract you
+from the actual code below. If you do dare to continue,
+please don't judge too hard.
+
+Oh, and LONG HAIL THE ZX SPECTRUM!
+
+/***************************************************/
+
+
+
+
+/* global document, window, alert, XMLHttpRequest, requestAnimationFrame */
 const element = document.querySelector('section.main');
 const root = document.documentElement;
-
+const ticketDate = '2017-07-20 10:00:00';
+console.log('Come back soon, I\'ll be doing more later…');
 const FREQ = 854; // Also speccy t-state for single bit
+
+const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
 
 const audioContext = new window.AudioContext();
 const gain = generateGain(); // we'll always connect to this for generated audio
 const toBinary = s => (parseInt(s, 16)).toString(2).padStart(8, '0');
+const bg = '#0518bc';
+const fg = '#f7fd38';
+const analyser = audioContext.createAnalyser();
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+analyser.fftSize = Math.pow(2, 8);
+gain.connect(analyser);
+
+function drawLoading(dataArray) {
+  // const bufferLength = dataArray.length;
+  const slice = (canvas.height / (bufferLength)) * 4; // max value is 2bytes
+
+  // reset the canvas
+  ctx.fillStyle = bg;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // ctx.lineWidth = 1;
+  // ctx.strokeStyle = fg;
+
+  ctx.fillStyle = fg;
+  ctx.beginPath();
+
+  let y = 0;
+  let paint = false;
+  let lastPulse = 0;
+  const offset = 0; //shift ? (bufferLength * .25) | 0 : 0;
+  const end = bufferLength; // * .75;
+
+  for (let i = offset + y; i < end; i++) {
+    const pulse = dataArray[i] / 128.0 | 0; // v is between 0 2 (0 being -1db and 2 being 1db)
+    let v = slice;
+
+    if (pulse < 1) {
+      v = slice/2;
+    } else {
+      ctx.rect(-1, y, canvas.width + 2, slice);
+    }
+
+    if (lastPulse !== v) {
+      paint = !paint;
+      lastPulse = v;
+    }
+
+    y += slice; //v;
+  }
+
+  ctx.fill();
+  ctx.fillStyle = bg;
+  ctx.beginPath();
+
+  // now fill in the centre box
+  // let x = canvas.width / 8;
+  // y = canvas.height / 6;
+  // const w = canvas.width - x*2;
+  // const h = canvas.height - y*2;
+  // ctx.fillStyle = 'rgb(200, 200, 200)';
+  // ctx.fillRect(x - .5, y, w, h);
+
+}
 
 function fileToBinary(file) {
   return new Promise(resolve => {
@@ -61,14 +159,15 @@ function play(data) {
   let ptr = 0;
   const RATE = 44100; //4098; //audioContext.sampleRate;
   // const REAL_TIME_FREQUENCY = 855; // this changes based on the 0 and 1 - 860 for 1, 1720 for 2
-  const ONE = 855;
-  const ZERO = ONE * 2;
+  const ZERO = 855;
+  const ONE = ZERO * 2;
   let REAL_TIME_FREQUENCY = data[ptr] == 1 ? ONE : ZERO;
   let ANGULAR_FREQUENCY = REAL_TIME_FREQUENCY * 2 * Math.PI;
 
   const FACTOR = 2; // 0 = 265 (smallest sample rate), 6 = 16384 (largest sample rate)
   const src = audioContext.createScriptProcessor(256 * Math.pow(2, FACTOR), 1, 1);
 
+  let toprint = [REAL_TIME_FREQUENCY === ONE ? 0 : 256];
   // Give the node a function to process audio events
   src.onaudioprocess = (audioProcessingEvent) => {
     // The input buffer is the song we loaded earlier
@@ -81,12 +180,11 @@ function play(data) {
     const channel = 0;
     const outputData = outputBuffer.getChannelData(channel);
 
+
     // Loop through the samples rate
     for (let sample = 0; sample < inputBuffer.length; sample++) {
-
       if (ptr > data.length) {
         src.disconnect();
-        gain.disconnect();
         return;
       }
 
@@ -103,15 +201,19 @@ function play(data) {
             // console.log('switch to 1 - slow', i);
           }
           ANGULAR_FREQUENCY = REAL_TIME_FREQUENCY * 2 * Math.PI;
+//           toprint.push([REAL_TIME_FREQUENCY === ONE ? 0 : 256]);
         } else if (ANGULAR_FREQUENCY !== 0) {
           ANGULAR_FREQUENCY = 0;
           src.disconnect();
-          gain.disconnect();
           return;
-          break;
         }
       }
     }
+
+//     if (toprint.length > 24) {
+//       drawLoading(toprint);
+//       toprint.length = 0;
+//     }
   };
 
   src.connect(gain);
@@ -131,7 +233,7 @@ function generateGain() {
 const generateSample = (sampleNumber, RATE, ANGULAR_FREQUENCY) => {
   const sampleTime = sampleNumber / RATE;
   const sampleAngle = sampleTime * ANGULAR_FREQUENCY;
-  const noise = 0; //Math.random() * 0.05;
+  const noise = Math.random() * 0.05;
   return Math.sin(sampleAngle) < 0 ? (-1 + noise) : (1 - noise);
 };
 
@@ -158,44 +260,46 @@ function BufferLoader(context, urlList, callback) {
   this.loadCount = 0;
 }
 
-BufferLoader.prototype.loadBuffer = function(url, index) {
+BufferLoader.prototype.loadBuffer = function (url, index) {
   // Load buffer asynchronously
   const request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  request.responseType = "arraybuffer";
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
 
-  var loader = this;
+  const loader = this;
 
-  request.onload = function() {
+  request.onload = () => {
     // Asynchronously decode the audio file data in request.response
     loader.context.decodeAudioData(
       request.response,
-      function(buffer) {
+      buffer => {
         if (!buffer) {
           alert('error decoding file data: ' + url);
           return;
         }
         loader.bufferList[index] = buffer;
-        if (++loader.loadCount == loader.urlList.length)
+        if (++loader.loadCount === loader.urlList.length) {
           loader.onload(loader.bufferList);
+        }
       },
-      function(error) {
+      error => {
         console.error('decodeAudioData error', error);
       }
     );
-  }
+  };
 
-  request.onerror = function() {
+  request.onerror = () => {
     alert('BufferLoader: XHR error');
-  }
+  };
 
   request.send();
-}
+};
 
-BufferLoader.prototype.load = function() {
-  for (let i = 0; i < this.urlList.length; ++i)
-  this.loadBuffer(this.urlList[i], i);
-}
+BufferLoader.prototype.load = function () {
+  for (let i = 0; i < this.urlList.length; ++i) {
+    this.loadBuffer(this.urlList[i], i);
+  }
+};
 
 function run(s, bufferList) {
   return new Promise(resolve => {
@@ -212,7 +316,9 @@ function run(s, bufferList) {
       sound.start(0);
     }
 
-    if (chr) str += '<span class="chr">L</span>';
+    if (chr) {
+      str += '<span class="chr">L</span>';
+    }
     element.innerHTML = str;
 
     setTimeout(resolve, delay);
@@ -222,7 +328,7 @@ function run(s, bufferList) {
 
 function sequencer(s, bufferList, done) {
   s.reduce((acc, curr) => {
-    return acc.then(() => run(curr, bufferList))
+    return acc.then(() => run(curr, bufferList));
   }, Promise.resolve()).then(done);
 }
 
@@ -239,8 +345,8 @@ function main(bufferList) {
       // TODO show count down to event
     ];
     setTimeout(() => {
-      sequencer(sequence, bufferList, startAudio)
-    }, 2000)
+      sequencer(sequence, bufferList, startAudio);
+    }, 2000);
   }, 2000);
 }
 
@@ -255,11 +361,11 @@ function startAudio() {
     loadimage(document.querySelector('img'));
   }, 4000);
 
-  const target = new Date('2017-07-20 10:00:00').getTime();
+  const target = new Date(ticketDate).getTime();
   const draw = () => {
     const delta = target - Date.now();
     if (delta <= 0) {
-      element.innerHTML = `R Tape loading error, 0:1<br>`;
+      element.innerHTML = 'R Tape loading error, 0:1<br>';
       return;
     }
     requestAnimationFrame(draw);
@@ -269,14 +375,14 @@ function startAudio() {
     let [d, h] = _h.split('T');
     h = (parseInt(d, 10) * 24 - 24) + parseInt(h, 10);
 
-    element.innerHTML = `Loading, please wait...<br><br>Available in ${h}:${m}:${s}`;
+    element.innerHTML = `Loading, please wait...<br><br>Ready in T-${h}:${m}:${s}`;
+
+    analyser.getByteTimeDomainData(dataArray);
+    drawLoading(dataArray);
   };
 }
 
 const bufferLoader = new BufferLoader(audioContext, ['/chr.m4a'], main);
 
-window.onload = () => {
-  bufferLoader.load();
-};
-
-console.log('Come back soon, I\'ll be doing more later…');
+// wait for our image to load first
+window.onload = () => bufferLoader.load();
