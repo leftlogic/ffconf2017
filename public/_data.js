b83938c14b1dc4326dfc35cf38b1e25040251ca3
Remy Sharp <remy@remysharp.com>
2017-09-26 14:14:10 +0100

    fix: working on speaker images

--- public/_data.js
+++ public/_data.js
@@ -155,9 +155,9 @@ module.exports = {
         duration: 40,
         title: 'Rethinking the Web Platform',
         speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
+          name: 'James Kyle',
+          twitter: 'thejameskyle',
+          photo: 'james.gif',
         },
         description:
           'Evolving the web to improve education, accessibility, performance, productivity, and design.',
@@ -168,38 +168,85 @@ module.exports = {
         audio: '',
         video: '',
       },
+
       {
         duration: 40,
-        title: 'Abstract art in a time of minification',
-        description:
-          'aesthetic is a major component of any medium for art, including the web, but one thing that has been bothering me lately is: what happened to "view source"? are we destroying aesthetic for the sake of tooling and in spite of access to our industry????¿¿¿¿',
+        title:
+          'If you\'re going out of San Francisco, be sure to wear Web Standards in your hair',
         speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
+          name: 'Bruce Lawson',
+          twitter: 'brucel',
+          photo: 'bruce.gif',
         },
+        description:
+          'The Web\'s evolving. That\'s nothing new - it\'s always done that. But now it\'s going to more and more people, in Africa, Asia, on weird and wonderful devices over weird and unwonderful networks. Meanwhile, frameworks spring up faster than dung beetles on a fresh poo; the CSS folks hate the React gang who hate the Angular crew. Yet we all want to make the web better - but what do we need it to be like? Let\'s look at the needs and challenges of the next 4 billion consumers, and work out what to do next.',
         bio:
-          'my kid laptop had a hangman game written in BASIC and i accessed the source and rigged it to cheat against my half sister!',
+          '1981, my Physics teacher made up a UK101 kit in a tiny computer room which I could use instead of Physical Exercise. So I programmed games.',
         links: [],
         slides: '',
         audio: '',
         video: '',
       },
+
       {
         break: true,
         title: 'Coffee break (30 mins)',
         duration: 30,
         svg: 'coffee',
       },
+
+      {
+        duration: 40,
+        title: 'How the web sausage gets made',
+        speaker: {
+          name: 'Monica Dinculescu',
+          twitter: 'notwaldorf',
+          photo: 'monica.gif',
+        },
+        description:
+          'They say there\'s two things you never want to see made: sausages and web standards. Sooooo I\'m going to tell you about both! How do browsers work? What are web components even? Everyone\'s using them and maybe you should too! 🆒🐱🎉',
+        bio:
+          'I found out my mum always won at the QBasic Gorrilas game bc she changed the other player\'s code to randomly miss. Then she taught me how.',
+        links: [],
+        slides: '',
+        audio: '',
+        video: '',
+      },
+
+      {
+        duration: 40,
+        title: 'Lessons learned sciencing the web',
+        description:
+          'Discover what slows down modern apps on mobile and how to fix this.',
+        speaker: {
+          name: 'Addy Osmani',
+          twitter: 'addyosmani',
+          photo: 'addy.gif',
+        },
+        bio:
+          'At 16, wrote a web browser in C++ to understand how HTML, CSS and JavaScript worked…15 years later, still learning',
+        links: [],
+        slides: '',
+        audio: '',
+        video: '',
+      },
+
+      {
+        break: true,
+        duration: 90,
+        title: 'Lunch break (90 mins)',
+        svg: 'lunch',
+      },
+
       {
         duration: 40,
         title: 'My Password Doesn\'t Work!',
         description:
           'Security is important, but it doesn\'t have to be complex. Let\'s dispel myths and assuage fear associated with those linchpin of our online lives – passwords – and build toward a more secure and more usable web.',
         speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
+          name: 'Blaine Cook',
+          twitter: 'blaine',
+          photo: 'blaine.gif',
         },
         bio:
           'A glowing green screen, still fascinating despite its sole output: "SYNTAX ERROR"; Years later, the moment I used ATA and ATDT to talk to a friend.',
@@ -214,9 +261,9 @@ module.exports = {
         description:
           'JavaScript does a remarkable job of hiding memory management from us. What\'s going on behind the scenes?',
         speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
+          name: 'Katie Fenn',
+          twitter: 'katie_fenn',
+          photo: 'katie.gif',
         },
         bio:
           'In 2001 I started making post signatures for a Final Fantasy forum, and then started coding new features for the forum itself.',
@@ -225,79 +272,41 @@ module.exports = {
         audio: '',
         video: '',
       },
-      {
-        break: true,
-        duration: 90,
-        title: 'Lunch break (90 mins)',
-        svg: 'lunch',
-      },
-      {
-        duration: 40,
-        title: 'How the web sausage gets made',
-        speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
-        },
-        description:
-          'They say there\'s two things you never want to see made: sausages and web standards. Sooooo I\'m going to tell you about both! How do browsers work? What are web components even? Everyone\'s using them and maybe you should too! 🆒🐱🎉',
-        bio:
-          'I found out my mum always won at the QBasic Gorrilas game bc she changed the other player\'s code to randomly miss. Then she taught me how.',
-        links: [],
-        slides: '',
-        audio: '',
-        video: '',
-      },
-      {
-        duration: 40,
-        title:
-          'If you\'re going out of San Francisco, be sure to wear Web Standards in your hair',
-        speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
-        },
-        description:
-          'The Web\'s evolving. That\'s nothing new - it\'s always done that. But now it\'s going to more and more people, in Africa, Asia, on weird and wonderful devices over weird and unwonderful networks. Meanwhile, frameworks spring up faster than dung beetles on a fresh poo; the CSS folks hate the React gang who hate the Angular crew. Yet we all want to make the web better - but what do we need it to be like? Let\'s look at the needs and challenges of the next 4 billion consumers, and work out what to do next.',
-        bio:
-          '1981, my Physics teacher made up a UK101 kit in a tiny computer room which I could use instead of Physical Exercise. So I programmed games.',
-        links: [],
-        slides: '',
-        audio: '',
-        video: '',
-      },
+
       {
         break: true,
         duration: 30,
         title: 'Cake break (30 mins)',
         svg: 'coffee',
       },
+
       {
         duration: 40,
-        title: 'Lessons learned sciencing the web',
+        title: 'Abstract art in a time of minification',
         description:
-          'Discover what slows down modern apps on mobile and how to fix this.',
+          'aesthetic is a major component of any medium for art, including the web, but one thing that has been bothering me lately is: what happened to "view source"? are we destroying aesthetic for the sake of tooling and in spite of access to our industry????¿¿¿¿',
         speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
+          name: 'Jenn Schiffer',
+          twitter: 'jennschiffer',
+          photo: 'jenn.gif',
         },
         bio:
-          'At 16, wrote a web browser in C++ to understand how HTML, CSS and JavaScript worked…15 years later, still learning',
+          'my kid laptop had a hangman game written in BASIC and i accessed the source and rigged it to cheat against my half sister!',
         links: [],
         slides: '',
         audio: '',
         video: '',
       },
+
       {
         duration: 40,
         title: 'Alpha, Beta, Gamer: Dev Mode',
         description:
           'A live performance of video games and stand up comedy from comedian and coder, including pre prepared web games to play and even creating a video game with the audience on stage in only 10 minutes.',
         speaker: {
-          name: '',
-          twitter: '',
-          photo: '',
+          name: 'Joe Hart',
+          twitter: 'joehart',
+          photo: 'joe.gif',
         },
         bio:
           'Parents got me Lego Mindstorms for Xmas, used it and got frustrated that the software didn\'t let me do exactly what I wanted. Thus here I am',
